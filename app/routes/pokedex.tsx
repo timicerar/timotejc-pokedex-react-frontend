54fa7108d0bf53fc0bea45e7aa22be68f6d16913
Timotej Cerar <timi.cerar@gmail.com>
2026-09-17 21:33:26 +0200

    update: small css updates

--- app/routes/pokedex.tsx
+++ app/routes/pokedex.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import Button from '~/components/components/Button/Button';
 import Card from '~/components/components/Card/Card';
 import ThemeToggle from '~/components/components/ThemeToggle/ThemeToggle';
 import Typography from '~/components/components/Typography/Typography';
@@ -22,6 +23,12 @@ const Pokedex = () => {
         </Typography>
       </Card>
       <ThemeToggle />
+      <Button variant="primary">{t('shared.pokedex')}</Button>
+      <Button variant="destructive" disabled>
+        {t('shared.pokedex')}
+      </Button>
+      <Button variant="ghost">{t('shared.pokedex')}</Button>
+      <Button variant="secondary">{t('shared.pokedex')}</Button>
     </div>
   );
 };
