3659d9b9a3d013bfdf401c516d0d821a5141737a
Timotej Cerar <timi.cerar@gmail.com>
2026-09-20 23:16:11 +0200

    update: update tokens and pokemon card to make it accessible with aria-label

--- app/components/compositions/PokemonCard/PokemonCard.tsx
+++ app/components/compositions/PokemonCard/PokemonCard.tsx
@@ -1,10 +1,12 @@
 import classNames from 'classnames';
+import { useTranslation } from 'react-i18next';
 import { useInView } from 'react-intersection-observer';
 import { usePokemon } from '~/api/pokemon/hooks';
 import Card from '~/components/components/Card/Card';
 import PokemonContent from '~/components/compositions/PokemonCard/Components/PokemonContent/PokemonContent';
 import PokemonContentSkeleton from '~/components/compositions/PokemonCard/Components/PokemonContentSkeleton/PokemonContentSkeleton';
 import type { PokemonCardProps } from '~/components/compositions/PokemonCard/PokemonCard.interface';
+import { formatDashedLabel } from '~/utils/stringUtils';
 import classes from './PokemonCard.module.scss';
 
 const PokemonCard = ({
@@ -14,6 +16,7 @@ const PokemonCard = ({
   hideBadges,
   priority,
 }: PokemonCardProps) => {
+  const { t } = useTranslation();
   const { data: cachedPokemon } = usePokemon({ name }, { enabled: false });
 
   const { ref, inView } = useInView({
@@ -29,6 +32,9 @@ const PokemonCard = ({
       fullWidth
       active={active}
       onClick={onClick}
+      aria-label={t('pokemonCard.viewDetails', {
+        name: formatDashedLabel(name),
+      })}
       className={classNames(classes.card, { [classes.noBadges]: hideBadges })}
     >
       {shouldRenderContent ? (
