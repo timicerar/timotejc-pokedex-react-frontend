7d42c27025c7e87b42a2c221ec3ffe5ea15d4a32
Timotej Cerar <timi.cerar@gmail.com>
2026-09-24 00:41:36 +0200

    feat: add logic to close all modals if route changes

--- app/components/providers/ModalProvider/ModalProvider.tsx
+++ app/components/providers/ModalProvider/ModalProvider.tsx
@@ -1,7 +1,29 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router';
 import ConfirmationModal from '~/components/compositions/Modals/ConfirmationModal/ConfirmationModal';
 import PokemonDetailsModal from '~/components/compositions/Modals/PokemonDetailsModal/PokemonDetailsModal';
+import { ModalTypes } from '~/constants/modal-provider';
+import { closeModal } from '~/store/modals';
 
 const ModalProvider = () => {
+  const { pathname } = useLocation();
+  const isFirstRender = useRef(true);
+
+  const closeAllModals = useCallback(() => {
+    for (const type of Object.values(ModalTypes)) {
+      closeModal(type);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    closeAllModals();
+  }, [pathname, closeAllModals]);
+
   return (
     <>
       <PokemonDetailsModal />
